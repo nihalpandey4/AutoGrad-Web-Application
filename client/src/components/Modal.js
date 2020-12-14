0fceca171cbc05e3d944112100467cb56b974c8f
nihalpandey4 <rockingnihal1305@gmail.com>
2020-12-14 15:52:20 +0530

    student info form added

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import history from "./history";
+import "./modal.css";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
--- client/src/components/Pages/Student/AnswerSheet.js
+++ client/src/components/Pages/Student/AnswerSheet.js
@@ -1,20 +1,111 @@
 import React from "react";
 import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
 import {getTestForAssessment} from "../../../actions"
 import "./answerSheet.css";
 import Header from "../../Header";
+import Modal from "../../Modal";
+import Loader from "../../Loader";
 
 class AnswerSheet extends React.Component{
+
+    state={student:{
+        name:"",
+        rollno:"",
+        consent:""
+    },testPaper:null}
+    
     componentDidMount=()=>{
         this.props.getTestForAssessment(this.props.match.params.id);
     }
 
+    componentDidUpdate=()=>{
+        if(this.state.testPaper===null){
+            this.setState({testPaper:this.props.testPaper});
+        }
+    }
+    
+    updateStudentDetails=(property,value)=>{
+        let temp = this.state.student;
+        temp[property] = value;
+        this.setState({student:temp});
+    }
+
+    renderModalHeader = ()=>{
+        return (
+            <div className="customui" >
+                <div className="row">
+                    <div className="column">
+                        Topic :
+                    </div>
+                    <div className="column">
+                        {this.state.testPaper.topic}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="column">
+                        Time Limit : 
+                    </div>
+                    <div className="column">
+                        {this.state.testPaper.timeLimit} minutes
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    renderContent = ()=>{
+        return (
+            <div className="ui form">
+                <div className="two fields">
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" placeholder="Enter Name" value={this.state.student.name} onChange ={(e)=>this.updateStudentDetails("name",e.target.value)} />
+                    </div>
+                    <div className="field">
+                        <label>Roll Number</label>
+                        <input type="number" placeholder="Enter Roll Number" onChange ={(e)=>this.updateStudentDetails("rollno",e.target.value)} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    startTest =async ()=>{
+        await this.setState({student:{...this.state.student,consent:"yes"}});
+        console.log(this.state.student);
+    }
+
+    renderActions=()=>{
+        return(
+          <div className="actions" >
+            <button className="ui button green" onClick={()=>this.startTest()}>
+                Start Test
+            </button>
+            <Link to="/" className="ui button">
+                Go back to login
+            </Link>
+          </div>
+        )
+      }
+
+    renderComponent=()=>{
+        if(this.state.testPaper===null){
+            return <Loader />;
+        }
+        if(!this.state.student.consent){
+            return <Modal header={this.renderModalHeader()} actions = {this.renderActions()} content={this.renderContent()} />;
+        }
+        else{
+            return <Header time={this.props.testPaper.timeLimit} />;
+        }
+    }
+
     render(){
-        console.log(this.props.testPaper);
         return(
             <div className= "answerSheet">
-                <Header time={this.props.testPaper.timeLimit} />
+                {this.renderComponent()}
             </div>
         )
     }
