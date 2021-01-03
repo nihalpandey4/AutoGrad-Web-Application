d6c68f7f1444ddff5d0cc18b0104bd8a5959a4ba
nihalpandey4 <rockingnihal1305@gmail.com>
2021-01-03 23:50:01 +0530

    form header made generic

--- client/src/components/AssessmentHeader.js
+++ client/src/components/AssessmentHeader.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Timer } from "react-countdown-clock-timer";
 
 import "./AssessmentHeader.css";
 
@@ -7,29 +6,14 @@ const Header = (props) => {
   return (
     <div className="customHeader">
       <div className="ui header spacebetween segment">
-        <div>Max Marks : {props.testPaper.maxMarks}</div>
-        <div>{props.testPaper.topic}</div>
-        <div style={{ display: "flex" }}>
-          Time Remaining : &nbsp;
-          <Timer
-            durationInSeconds={props.testPaper.timeLimit * 60}
-            formatted={true}
-            isPaused={false}
-            onStart={() => {
-              alert(` You have ${props.testPaper.timeLimit} minutes`);
-            }}
-            onFinish={() => {
-              alert("Times up!");
-            }}
-          />
-        </div>
+        {props.upperHeader}
       </div>
 
       <div className="ui hidden divider"></div>
 
       <div className="ui spacebetween segment">
-        <div className="textWithbutton"> Attendee's Name : {props.student.name} </div>
-        <div className="textWithbutton"> Attendee's Roll no : {props.student.rollno} </div>
+        <div className="textWithbutton"> Student's Name : {props.student.name} </div>
+        <div className="textWithbutton"> Student's Roll no : {props.student.rollno} </div>
       </div>
       
     </div>
