9e8f6f72840d92231c59d2836db80e2f87935a94
nihalpandey4 <rockingnihal1305@gmail.com>
2021-01-03 18:50:09 +0530

    result structure for students created

--- client/src/components/Pages/Teachers/TestStats.js
+++ client/src/components/Pages/Teachers/TestStats.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import NavBar from "../../NavBar";
 import Modal from "../../Modal";
 import { getTestForTeacher } from "../../../actions";
 import Loader from "../../Loader";
+import List from "../../List";
 
 class TestStats extends React.Component {
-  state = { userId: null};
+  state = { userId: null };
 
   componentDidUpdate = () => {
     if (this.state.userId === null && this.props.userId !== this.state.userId) {
@@ -17,46 +18,93 @@ class TestStats extends React.Component {
     }
   };
 
+  renderListContent = (student) => {
+    console.log(student);
+    return (
+      <React.Fragment>
+        <div>
+          <span className="ui subheading">Roll No = {student.rollno}</span>
+        </div>
+        <div>
+          <span className="ui subheading">Status = Unchecked</span>
+        </div>
+        <div>
+          <span className="ui subheading">Marks = N/A</span>
+        </div>
+      </React.Fragment>
+    );
+  };
+
+  renderListRightContent=(student)=>{
+    return (
+      <React.Fragment>
+        <button className="ui blue button">Evaluate</button>
+      </React.Fragment>
+    )
+  }
+
   renderModalHeader = () => {
     return (
       <div className="customui">
         <div className="row">
           <div className="column">Topic : {this.props.testPaper.topic}</div>
           <div className="column">
-            Attempted by - {" "+this.props.testPaper.attemptedBy} student(s)
+            Attempted by - {" " + this.props.testPaper.attemptedBy} student(s)
           </div>
         </div>
       </div>
     );
   };
 
-  renderModalActions=()=>{
-    return(
-      <div className="actions" >
-        <button className="ui disabled button blue" >
-            Show selected student
-        </button>
+  renderModalContent = () => {
+    return (
+      <>
+        <List
+          name="Students' stats"
+          items={this.props.testPaper.students}
+          content={this.renderListContent}
+          rightContent = {this.renderListRightContent}
+          type="justified"
+        />
+      </>
+    );
+  };
+
+  renderModalActions = () => {
+    return (
+      <div className="actions">
+        {/* <button className="ui disabled button blue">
+          Show selected student
+        </button> */}
         <Link to="/" className="ui button">
-            Cancel
+          Cancel
         </Link>
       </div>
-    )
-  }
+    );
+  };
 
   renderContent = () => {
-    if (this.props.testPaper==null) {
+    if (this.props.testPaper == null) {
       return <Loader />;
     }
-    return <Modal header={this.renderModalHeader()} actions={this.renderModalActions()}  />;
+    return (
+      <Modal
+        header={this.renderModalHeader()}
+        actions={this.renderModalActions()}
+        content={this.renderModalContent()}
+      />
+    );
   };
 
   render = () => {
     return (
+      <div>
+        {this.renderContent()}
         <div>
-          {this.renderContent()}
-          <div><NavBar/></div>
+          <NavBar />
         </div>
-    )
+      </div>
+    );
   };
 }
 
